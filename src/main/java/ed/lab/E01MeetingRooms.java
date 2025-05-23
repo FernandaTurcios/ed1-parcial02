205847aadcc0bc0dc59d52728d9e1649ca39e052
FernandaTurcios <jfturcios@correo.url.edu.gt>
2025-05-23 07:41:07 -0600

    Ejercicio1

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,27 @@
 package ed.lab;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
+        }
 
+        meetingIntervals.sort((a, b) -> Integer.compare(a.startTime(), b.startTime()));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (MeetingInterval interval : meetingIntervals) {
+            if (!minHeap.isEmpty() && minHeap.peek() <= interval.startTime()) {
+                minHeap.poll();
+            }
+
+            minHeap.offer(interval.endTime());
+        }
+
+        return minHeap.size();
+    }
 }
